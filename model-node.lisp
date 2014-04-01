20f0e82068cc8100ac5e248d7e12190f176ee19a
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2014-04-01 18:02:29 +0200

    Fixed run-program when available in sbcl or asdf

--- model-node.lisp
+++ model-node.lisp
@@ -120,9 +120,12 @@
 
 (defun files-equal (file1 file2)
   "Compare files using external diff. Returns t if equal"
-  (let ((f1 (namestring (file-exists-p file1)))
-        (f2 (namestring (file-exists-p file2))))
-    (not (asdf/run-program:run-program (list "diff" "-q" f1 f2) :ignore-error-status t :output :lines))))
+  (let* ((f1 (namestring (file-exists-p file1)))
+         (f2 (namestring (file-exists-p file2)))
+         (cmd (if (fboundp 'asdf/run-program:run-program)
+                  `(asdf/run-program:run-program (list "diff" "-q" ,f1 ,f2) :ignore-error-status t :output :lines)
+                  `(with-output-to-string (out) (sb-ext:run-program "diff" '("-q" ,f1 ,f2) :output out :search t)))))
+    (not (eval cmd))))
 
 
 (defun diff-node-partial-rescan (node)
