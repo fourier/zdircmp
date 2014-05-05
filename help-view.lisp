30949af4bb535a74cc4bce28d0bd684f41e2269a
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2014-05-05 18:57:58 +0200

    Use 'point' as in Emacs instead of 'cursor'

--- help-view.lisp
+++ help-view.lisp
@@ -31,69 +31,71 @@
 
 (in-package :zdircmp.view.help)
 
-(defstruct cursor
-  "Cursor position. LINE is the line number in window, COLUMN is the column.
+(defstruct point
+  "Point position. LINE is the line number in window, COLUMN is the column.
 Both 0-based"
   (line 0)
   (column 0))
 
 
 (defclass help-view (view)
-  ((cursor-pos :initform (make-cursor)
-               :accessor cursor-pos
-               :documentation "Current cursor position"))
+  ((point-pos :initform (make-point)
+               :accessor point-pos
+               :documentation "Current point position"))
    (:documentation "Help window class"))
 
 
-(defgeneric goto-pos (v &key line col))
-(defmethod goto-pos ((v help-view) &key (line (cursor-line (cursor-pos v)))
-                                     (col (cursor-column (cursor-pos v))))
-  (setf (cursor-line (cursor-pos v)) line)
-  (setf (cursor-column (cursor-pos v)) col))
+(defgeneric goto-point (v &key line col))
+(defmethod goto-point ((v help-view) &key (line (point-line (point-pos v)))
+                                     (col (point-column (point-pos v))))
+  (setf (point-line (point-pos v)) line)
+  (setf (point-column (point-pos v)) col))
 
 
-(defgeneric print-string (v string &key with-color))
-(defmethod print-string ((v help-view) string &key (with-color :white))
+(defgeneric print-string (v string &key with-color line col))
+(defmethod print-string ((v help-view) string &key (with-color :white) line col)
   (with-window v w
-               (let ((line (cursor-line (cursor-pos v)))
-                     (col  (cursor-column (cursor-pos v)))
+               (let ((l (if line line 
+                            (point-line (point-pos v))))
+                     (c (if col col (point-column (point-pos v))))
                      (size (length string)))
+                 (goto-point v :line l :col c )
                  (with-color-win w with-color
-                                 (mvwprintw w line col string))
-                 (goto-pos v :col (+ col size)))))
+                                 (mvwprintw w l c string))
+                 (goto-point v :col (+ c size)))))
 
 (defmethod refresh :before ((v help-view) &key (force t))
   (declare (ignore force))
-  (goto-pos v :line 0 :col 0)
+  (goto-point v :line 0 :col 0)
   (print-string v "Directory tree differences report tool.")
-  (goto-pos v :line 1 :col 0)
+  (goto-point v :line 1 :col 0)
   (print-string v "Navigation: ")
   (print-string v "LEFT/RIGHT/UP/DOWN" :with-color :green)
   (print-string v " and ")
   (print-string v "TAB" :with-color :green)
   (print-string v " to switch between panes")
-  (goto-pos v :line 2 :col 0)
+  (goto-point v :line 2 :col 0)
   (print-string v "Press ")
   (print-string v "ESC" :with-color :green)
   (print-string v " to exit")
-  (goto-pos v :line 3 :col 0)
+  (goto-point v :line 3 :col 0)
   (print-string v "Press ")
   (print-string v "ENTER" :with-color :green)
   (print-string v " to open/close directories or start ")  
   (print-string v "vimdiff" :with-color :red)
   (print-string v " on different files")
-  (goto-pos v :line 4 :col 0)
+  (goto-point v :line 4 :col 0)
   (print-string v "Press ")
   (print-string v "BACKSPACE" :with-color :green)
   (print-string v " to jump up in tree or close current directory")
-  (goto-pos v :line 5 :col 0)
+  (goto-point v :line 5 :col 0)
   (print-string v "Legend:")
-  (goto-pos v :line 6 :col 0)
+  (goto-point v :line 6 :col 0)
   (print-string v "\"file name\" - files/dirs are the same")
-  (goto-pos v :line 7 :col 0)
+  (goto-point v :line 7 :col 0)
   (print-string v "\"file name\"" :with-color :red)
   (print-string v " - files/dirs are different")
-  (goto-pos v :line 8 :col 0)
+  (goto-point v :line 8 :col 0)
   (print-string v "\"file name\"" :with-color :cyan)
   (print-string v " - files(or contents of dir) exist only on one pane"))
   
@@ -103,7 +105,7 @@ Both 0-based"
     (mvwprintw w 0 0 "Directory tree differences report tool.")
     (mvwprintw w 1 0 "Navigation: ")
     (with-color-win w :green
-                    (mvwprintw w 1 (length "Navigation: ") "Cursor keys and TAB"))
+                    (mvwprintw w 1 (length "Navigation: ") "Point keys and TAB"))
     (mvwprintw w 2 0 "Press")
     (with-color-win w :green
                     (mvwprintw w 2 (length "Press ") "ESC"))))
